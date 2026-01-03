7bfdd9d425fa9aa88efe63777d1b72556d07bb96
Mounesh v <mouneshv696@gmail.com>
2026-01-03 13:07:37 +0530

    Admin Apllications

--- backend/src/models/AutoLoan.js
+++ backend/src/models/AutoLoan.js
@@ -1,67 +1,84 @@
 import mongoose from "mongoose";
 
 const autoloanSchema = new mongoose.Schema(
-    {
-        //User reference
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-
-        loanType: {
-            type: String,
-            default: "home",
-            enum: ["home"],
-        },
-        //personal information ;
-        fullName:{
-            type: String,
-            required: [true, "Full name is required"],
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            trim: true,
-            lowercase: true,
-
-        },
-        phone: {
-         type: String,
-         required: [true, "Phone number is required"],
-         trim: true,
-        },
-        vehicleType: {
-            type: String,
-            enum: ["car", "Bike", "Scooter"],
-            required: [true, "vehicleType is required"],
-
-        },
-        vehiclePrice:{
-            required: [true, "vehicle price is required"],
-            min: [0,"Price above 0"],
-        },
-        loanAmount: {
-            type: Number,
-            required: [true, "loanAmount is required"],
-            enum:[1000, "Loan Amount is required"],
-
-        },
-        //Application status,
-        status: {
-            type: String,
-            enum: ["pending", "under-review", "approved", "rejected"],
-            default: "pending",
-        },
-        adminNotes: {
-            type:String,
-            default: "",
-        },
-
+  {
+    // User reference
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps: true,  //Automatically adds createdAt and updatedAt
-    }
+
+    loanType: {
+      type: String,
+      default: "auto",
+      enum: ["auto"],
+    },
+
+    // Personal information
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      lowercase: true,
+    },
+
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+    },
+
+    vehicleType: {
+      type: String,
+      enum: ["car", "bike", "scooter"],
+      required: [true, "Vehicle type is required"],
+    },
+
+    vehiclePrice: {
+      type: Number,
+      required: [true, "Vehicle price is required"],
+      min: [1, "Vehicle price must be greater than 0"],
+    },
+
+    loanAmount: {
+      type: Number,
+      required: [true, "Loan amount is required"],
+      min: [1000, "Loan amount must be at least 1000"],
+    },
+
+    employmentStatus: {
+      type: String,
+      enum: ["Salaried", "Self-Employed", "Business"],
+      required: [true, "Employement Status is required"],
+    },
+
+    monthlyIncome: {
+      type: Number,
+      required: [true, "Monthly income is required"],
+    },
+
+    // Application status
+    status: {
+      type: String,
+      enum: ["pending", "under-review", "approved", "rejected"],
+      default: "pending",
+    },
+
+    adminNotes: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
 export default mongoose.model("AutoLoan", autoloanSchema);
--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -2,12 +2,17 @@ import express from "express";
 import loginAdmin from "../controllers/adminController.js";
 import { changeAdminPassword, getAdminEmail } from "../utils/CreateAdmin.js";
 import authAdmin from "../middleware/admin.js";
+import { getAllHomeApplications } from "../controllers/applicationController.js";
 
 const router = express.Router();
 
 // POST /api/admin/admin-login - Admin login
 router.post("/admin-login", loginAdmin);
-router.get("/admin-email",authAdmin, getAdminEmail);
+
+router.get("/admin-email", authAdmin, getAdminEmail);
 router.put("/change-password", authAdmin, changeAdminPassword);
+
+// GET All Home Applications Only Admin
+router.get("/All-Home-Application", authAdmin, getAllHomeApplications);
 
 export default router;
